f65a9fb89c9412763252f24b58fc6d960d76194d
preawa <preawa.darasawat46@gmail.com>
2022-10-18 21:03:34 +0700

    b 4

--- resources/views/map1.blade.php
+++ resources/views/map1.blade.php
@@ -253,7 +253,7 @@
                                                         @foreach ($cats as $key => $cat)
                                                             @if ($cat->status != 0)
                                                                 <tr>
-                                                                    <td>{{  num + 1 }}</td>
+                                                                    <td>{{  $i=1 }}</td>
                                                                     {{-- <td>{{ $key + 1 }}</td> --}}
                                                                     <td>{{ str_limit($cat->name, '10') }}</td>
                                                                     <td>{{ $cat->user->name }}</td>
